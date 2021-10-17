02b967a043ea708747637d66ebf1b26d8104f1d8
pidokige02 <pidokige0204@gmail.com>
2021-10-17 22:20:25 +0900

    regular commit

--- time3.php
+++ time3.php
@@ -1,4 +1,6 @@
 <?php
 $timezones = ["Asia/Seoul", "America/New_York"];
 echo implode(',', $timezones);
+// 배열에 속한 문자열을 하나의 문자열로 만드는 함수
+// "Asia/Seoul,America/New_York"
 ?>
--- time4.php
+++ time4.php
@@ -2,4 +2,5 @@
 $timezones = ["Asia/Seoul", "America/New_York"];
 header('Content-Type: application/json');
 echo json_encode($timezones);
+// PHP Array 또는 String 따위를 JSON 문자열로 변환하는 PHP 함수
 ?>
